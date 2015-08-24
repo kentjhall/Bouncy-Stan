c614fa32afcca6e49e7cbd376a329b95268c48a4
kentjhall <kentjhall3@hotmail.com>
2015-08-24 01:29:48 -0400

    ads work!!! (still some improvements necessary)

--- ios/src/com/mygdx/game/IOSLauncher.java
+++ ios/src/com/mygdx/game/IOSLauncher.java
@@ -8,6 +8,9 @@ import org.robovm.apple.foundation.NSObject;
 import org.robovm.apple.foundation.NSString;
 import org.robovm.apple.uikit.UIApplication;
 import org.robovm.apple.uikit.UIScreen;
+import org.robovm.apple.uikit.UIView;
+import org.robovm.apple.uikit.UIViewController;
+import org.robovm.apple.uikit.UIWindow;
 
 import admob.src.org.robovm.bindings.admob.GADAdSize;
 import admob.src.org.robovm.bindings.admob.GADBannerView;
@@ -18,6 +21,8 @@ import admob.src.org.robovm.bindings.admob.GADRequestError;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.badlogic.gdx.backends.iosrobovm.IOSGraphics;
+import com.badlogic.gdx.backends.iosrobovm.IOSViewControllerListener;
 import com.badlogic.gdx.utils.Logger;
 import com.dugga.game.IActivityRequestHandler;
 import com.dugga.game.MyGdxGame;
@@ -27,6 +32,8 @@ public class IOSLauncher extends IOSApplication.Delegate implements IActivityReq
     private GADBannerView adview;
     private boolean adsInitialized = false;
     private IOSApplication iosApplication;
+    private UIViewController viewController;
+    private UIWindow window;
     private float bannerWidth;
     private float bannerHeight;
     public double screenWidth;
@@ -39,21 +46,24 @@ public class IOSLauncher extends IOSApplication.Delegate implements IActivityReq
         iosApplication = new IOSApplication(new MyGdxGame(this), config);
         config.orientationLandscape=false;
         config.orientationPortrait=true;
+        viewController=new UIViewController();
 
-        //log.debug("testing");
+        initializeAds();
 
-//        final CGSize screenSize = UIScreen.getMainScreen().getBounds().getSize();
-//        screenWidth = screenSize.getWidth();
+        final CGSize screenSize = UIScreen.getMainScreen().getBounds().getSize();
+        screenWidth = screenSize.getWidth();
 
-//        final CGSize adSize = adview.getBounds().getSize();
-//        adWidth = adSize.getWidth();
-//        adHeight = adSize.getHeight();
+        final CGSize adSize = adview.getBounds().getSize();
+        adWidth = adSize.getWidth();
+        adHeight = adSize.getHeight();
+
+        window=new UIWindow(adview.getBounds());
+        window.setRootViewController(viewController);
 
 //        log.debug(String.format("Hidding ad. size[%s, %s]", adWidth, adHeight));
 
-//        bannerWidth = (float) screenWidth;
-//        bannerHeight = (float) (bannerWidth / adWidth * adHeight);
-
+        bannerWidth = (float) screenWidth;
+        bannerHeight = (float) (bannerWidth / adWidth * adHeight);
 
         return iosApplication;
     }
@@ -66,12 +76,11 @@ public class IOSLauncher extends IOSApplication.Delegate implements IActivityReq
 
 
     public void initializeAds() {
-            log.debug("Initalizing ads...");
 
-            adview = new GADBannerView(GADAdSize.smartBannerPortrait());
-            adview.setAdUnitID("ca-app-pub-4743789296025031/7095109830"); //put your secret key here
-            adview.setRootViewController(iosApplication.getUIViewController());
-            iosApplication.getUIViewController().getView().addSubview(adview);
+        adview = new GADBannerView(GADAdSize.smartBannerPortrait());
+        adview.setAdUnitID("ca-app-pub-4743789296025031/7095109830"); //put your secret key here
+        adview.setRootViewController(viewController);
+        viewController.getView().addSubview(adview);
 
             final GADRequest request = GADRequest.create();
 //            if (USE_TEST_DEVICES) {
@@ -85,36 +94,31 @@ public class IOSLauncher extends IOSApplication.Delegate implements IActivityReq
                 @Override
                 public void didReceiveAd(GADBannerView view) {
                     super.didReceiveAd(view);
-                    log.debug("didReceiveAd");
                 }
 
                 @Override
                 public void didFailToReceiveAd(GADBannerView view,
                                                GADRequestError error) {
                     super.didFailToReceiveAd(view, error);
-                    log.debug("didFailToReceiveAd:" + error);
                 }
             });
 
             adview.loadRequest(request);
-
-            log.debug("Initalizing ads complete.");
-        }
+     }
 
     @Override
     public void showAds(adState show) {
 
-//        switch (show){
-//            case LOAD:
-//                adview.setFrame(new CGRect((screenWidth / 2) - adWidth / 2, 0, bannerWidth, bannerHeight));
-//                break;
-//            case SHOW:
-//                adview.setFrame(new CGRect((screenWidth / 2) - adWidth / 2, 0, bannerWidth, bannerHeight));
-//                break;
-//            case HIDE:
-//                adview.setFrame(new CGRect(0, -bannerHeight, bannerWidth, bannerHeight));
-//                break;
-//        }
+        switch (show){
+            case LOAD:
+                break;
+            case SHOW:
+                window.makeKeyAndVisible();
+                break;
+            case HIDE:
+                iosApplication.getUIWindow().makeKeyAndVisible();
+                break;
+        }
     }
 
 }
